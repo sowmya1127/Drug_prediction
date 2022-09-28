b3e51f35457b4c365c7fa4e5537bbc226ded9368
sowmya1127 <77708438+sowmya1127@users.noreply.github.com>
2022-09-28 11:36:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import Flask ,request,jsonify,render_template
 import pickle
 
-app=Flask(__name__,template_folder="templates")
+main=Flask(__name__,template_folder="templates")
 model=pickle.load(open('model.pkl','rb'))
 
 @app.route('/')
